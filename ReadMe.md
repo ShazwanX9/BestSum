1b6d58ace5e777867acd2c2bc2e89035eafbd2fd
Shazwan <76102465+ShazwanX9@users.noreply.github.com>
2021-11-01 01:50:47 +0800

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,24 @@
+# Choose Best Summation
+
+___
+    Find the best fit to lookup value                       (ref_val)
+    By calculating the sum of combination n number          (size) 
+    From list of integer provided                           (ls)
+___
+
+    Required:
+        :param:  -> int     - (ref_val)
+        :param:  -> int     - (size)
+        :param:  -> list    - (ls)
+        :return: -> int     - the nearest at most of int ref_val
+
+    Example:
+        choose_best_sum(39, 3, [14, 14, 3, 7, 14, 21, 5, 12, 23, 22, 16, 11, 23, 12, 16, 18, 1])
+            => (14, 14, 11), 39
+
+## DISCLAIMER!
+    no guarantee of completeness, accuracy, timeliness or 
+    of the results obtained from the use of this information
+
+### For more information:
+	Visit [Wikipedia](https://en.wikipedia.org/wiki/Combination)
---
+++ choose_best_sum.py
@@ -0,0 +1,31 @@
+from random import randrange
+from itertools import combinations
+
+def choose_best_sum(ref_val, size, ls):
+    comb, res = [], 0
+
+    for check_combination in combinations(ls, size):
+        check = sum(check_combination)
+
+        if check < ref_val and check > res:
+            comb, res = check_combination, check
+
+        elif check == ref_val:
+            return check_combination, check
+
+    return comb, res if res else None
+
+
+if __name__ == "__main__":
+    print("Testing for " + __file__)
+
+    TEST = 3
+    SIZE = 20
+    CASE = 50
+
+    result = lambda t,s,ls,rls,x: f"RESULT FOR `{t}` FOR SIZE `{s}` FROM LIST `{ls}` IS: \n\t => {rls} = `{x}`"
+
+    for i in range(TEST):
+        t, s = randrange(CASE), randrange(1,SIZE//4)
+        ref_ls = [randrange(1, CASE//2) for i in range(randrange(3, SIZE))]
+        print(result(t, s, ref_ls, *choose_best_sum(t, s, ref_ls)), '\n')
